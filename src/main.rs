6b28fcaee2d99baa996d8bca6298744dbe621431
asadsyria2018a-hub <asadsyria2018a@gmail.com>
2026-09-23 03:59:03 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use winit::platform::android::activity::AndroidApp;
 
 #[cfg(target_os = "android")]
 use winit::platform::android::EventLoopBuilderExtAndroid;
-fn run() -> Result<()> {
+fn run(android_app: Option<AndroidApp>) -> Result<()> {
     let log_path = match vera20k::util::logging::init_file_logger("ra2") {
         Ok(path) => {
             eprintln!("Logging to {}", path.display());
